80037780693031fb9491be76dbd2ff0958bfa9a5
unknown <alexchaguendo01@gmail.com>
2025-05-13 09:05:14 -0500

    crud realizado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ async function connectDB() {
 
       
         try {
-            const [results, metadata] = await db.query('SELECT * FROM relusuarioevento');
+            const [results, metadata] = await db.query('SELECT * FROM actividad');
             console.log('Datos de ejemplo:', results);
         } catch (error) {
             console.error('Error al ejecutar la consulta:', error);
