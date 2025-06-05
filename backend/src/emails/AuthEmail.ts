210af5c362fb0a20c72be353154c9717df01ff79
Alexjh117 <alexchaguendo01@gmail.com>
2025-06-05 18:16:09 -0500

    cambio de contraseña y validacion

--- backend/src/emails/AuthEmail.ts
+++ backend/src/emails/AuthEmail.ts
@@ -25,5 +25,24 @@ export class AuthEmail {
     } catch (error) {
       console.error(` Error al enviar correo a ${Usuario.Correo}:`, error);
     }
-  };
+  }
+  static sendContrasenaResetToken = async (Usuario: EmailType) => {
+    try {
+      const email = await transport.sendMail({
+        from: 'ActivSena_2025 <admin@activsena_2025.com>',
+        to: Usuario.Correo,
+        subject: 'ActivSena_2025 - Restablece tu contraseña',
+        html: `
+          <p>Hola: ${Usuario.Nombre}, has solicitado restablecer tu contraseña</p>
+          <p>Visita el siguiente enlace para confirmar tu cuenta:</p>
+          <a href="#">Restablecer Contraseña</a>
+          <p>O ingresa el siguiente código de verificación: <strong>${Usuario.token}</strong></p>
+        `
+      });
+
+      console.log(` Correo de confirmación enviado a ${Usuario.Correo}, ID del mensaje: ${email.messageId}`);
+    } catch (error) {
+      console.error(` Error al enviar correo a ${Usuario.Correo}:`, error);
+    }
+}
 }
